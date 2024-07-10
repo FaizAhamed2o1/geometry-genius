a2beb2963b94bfc8b62383d5ed8fd787af0ec76e
FaizAhamed2o1 <faizahmed23014@gmail.com>
2024-07-10 16:15:37 +0600

    area showing in card for all shapes

--- scripts/triangle-calculate.js
+++ scripts/triangle-calculate.js
@@ -40,3 +40,67 @@ function calculateRectangleArea() {
   document.getElementById("rectangle-height").value = "";
   document.getElementById("rectangle-width").value = "";
 }
+
+/**
+ * todo: making a reusable function for similar tasks
+ */
+
+// ? reusable function to get the input field's values
+function getInputValueById(inputFieldId) {
+  const inputField = parseFloat(document.getElementById(inputFieldId).value);
+
+  return inputField;
+}
+
+// ? reusable function to set the value of the inner text to show the area result
+function setAreaResult(areaId, areaValue) {
+  document.getElementById(areaId).innerText = areaValue;
+}
+
+// ? Reusable function to clear the input fields after calculating the result
+function clearInputField(fieldId) {
+  document.getElementById(fieldId).value = "";
+}
+
+// ! function to calculate the area of parallelogram
+function calculateParallelogramArea() {
+  const paraBase = getInputValueById("para-base");
+  const paraHeight = getInputValueById("para-height");
+
+  const paraArea = paraBase * paraHeight;
+  setAreaResult("para-area", paraArea);
+}
+
+// ! Function to calculate the area of rhombus
+function calculateRhombusArea() {
+  const rhombusD1 = getInputValueById("rhombus-d1");
+  const rhombusD2 = getInputValueById("rhombus-d2");
+
+  const rhombusArea = 0.5 * rhombusD1 * rhombusD2;
+  setAreaResult("rhombus-area", rhombusArea);
+}
+
+// ! Function to calculate the area of pentagon
+function calculatePentagonArea() {
+  const pentagonP = getInputValueById("pentagon-p");
+  const pentagonB = getInputValueById("pentagon-b");
+
+  const pentagonArea = 0.5 * pentagonP * pentagonB;
+  setAreaResult("pentagon-area", pentagonArea);
+  clearInputField("pentagon-p");
+  clearInputField("pentagon-b");
+}
+
+// ! function to calculate the area of ellipse
+function calculateEllipseArea() {
+  const pi = 3.1416;
+  const ellipseA = getInputValueById("ellipse-a");
+  const ellipseB = getInputValueById("ellipse-b");
+
+  const ellipseArea = pi * ellipseA * ellipseB;
+
+  setAreaResult("ellipse-area", ellipseArea);
+
+  clearInputField("ellipse-a");
+  clearInputField("ellipse-b");
+}
